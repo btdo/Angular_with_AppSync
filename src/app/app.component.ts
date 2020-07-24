1c6ace3164da6cb750178101090a199c8df69ecd
bao do <baodo210@hotmail.com>
2020-07-24 13:31:12 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { APIService } from './API.service';
+import Amplify, { Auth } from 'aws-amplify';
+import AWSAppSyncClient, { AUTH_TYPE } from 'aws-appsync';
+import awsconfig from '../aws-exports';
+import { listTodos } from './graphql/queries';
+import gql from 'graphql-tag';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'appsync-v1';
+  todos: Array<any>;
+
+  constructor(private apiService: APIService) {}
+
+  getOIDCToken = async () =>
+    await 'eyJraWQiOiI4WWhSaWRVRjUtNGpMVkFGVWw1V3ZIZU5ENUhGZXJPWDBxOEltY2hTUmZBIiwiYWxnIjoiUlMyNTYifQ.eyJydF9oYXNoIjoiekNRQjR6eW1FLW5QVlZYYUFKZEEzUSIsImlhdCI6MTU5NTYxMDU5MiwiaXNzIjoiaHR0cHM6Ly9zdGcwMDItd3d3LmVwb3N0LmNhL21nYS9zcHMvb2F1dGgvb2F1dGgyMC9tZXRhZGF0YS9jcGMtUk9QQyIsImF0X2hhc2giOiItTUNWNi1zVnp3eVpreXJUSGJXS3RRIiwic3ViIjoic2JzdGcybmFwcCIsImV4cCI6MTU5NTYxNzc5MiwiYXVkIjoiY3BjLVJPUEMtbW9iaWxlIn0.KC1HZcSyyL-8AMa9PxXyXnsXXKMXEjUn4Sv34ilX4DRdztZ_G3eeXmncntQBn226MpStxgbX7vxmW0O2BhOpf3RXmQ9XUy35aeJO3qz785axFlhU1eYK_VOPW-MMWc8CTxsaTxscO_LPjNyxoganKbiBwNnWdt9ERMCJJWd7zEuJOdCnGDZLa2tm6eSQui84FUNvsySw791e6uHCm0CK63z9u1wFR5jFToR9dkav76wTkbZd9zCoTWbybmL0gFL5WXvQMMrijhsm7C5s_nheth4JGL_R_xGa-4-Ux5o_85oez7826V9Wuttm6WVZfbUI9fgYLMcq47b06LSPJK08vQ'; // Should be an async function that handles token refresh
+
+  client = new AWSAppSyncClient({
+    url: awsconfig.aws_appsync_graphqlEndpoint,
+    region: awsconfig.aws_appsync_region,
+    auth: {
+      type: AUTH_TYPE.OPENID_CONNECT,
+      jwtToken: () => this.getOIDCToken(),
+    },
+  });
+
+  async ngOnInit() {
+    // this.apiService.ListTrackItems().then((evt) => {
+    //   this.todos = evt.items;
+    // });
+
+    // this.apiService.OnCreateTrackItemListener.subscribe((evt) => {
+    //   const data = (evt as any).value.data.onCreateTodo;
+    //   this.todos = [...this.todos, data];
+    // });
+
+    this.client
+      .query({
+        query: gql(listTodos),
+      })
+      .then(({ data: { listTodos } }) => {
+        console.log(listTodos.items);
+      });
+  }
+
+  createTodo() {
+    // this.apiService.CreateTrackItem({
+    //   pin: 'Angular',
+    //   description: 'testing',
+    // });
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,19 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import PubSub from '@aws-amplify/pubsub';
+import API from '@aws-amplify/api';
+import awsconfig from './aws-exports';
+import Amplify, { Auth } from 'aws-amplify';
+import AWSAppSyncClient, { AUTH_TYPE } from 'aws-appsync';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
+
+API.configure(awsconfig);
+PubSub.configure(awsconfig);
