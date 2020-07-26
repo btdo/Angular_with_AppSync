4fc8ec063cfba1032c18d5e32b341d4b53fdf6d0
bao do <baodo210@hotmail.com>
2020-07-26 15:57:20 -0400

    got subscription to work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent implements OnInit {
   constructor(private apiService: APIService) {}
 
   getOIDCToken = async () =>
-    await 'eyJraWQiOiI4WWhSaWRVRjUtNGpMVkFGVWw1V3ZIZU5ENUhGZXJPWDBxOEltY2hTUmZBIiwiYWxnIjoiUlMyNTYifQ.eyJydF9oYXNoIjoiY01OVWE3bU5XZ05odFJ4bm5uVXcwUSIsImlhdCI6MTU5NTc3MTU4OSwiaXNzIjoiaHR0cHM6Ly9zdGcwMDItd3d3LmVwb3N0LmNhL21nYS9zcHMvb2F1dGgvb2F1dGgyMC9tZXRhZGF0YS9jcGMtUk9QQyIsImF0X2hhc2giOiJvdV9hak4ta0dlUGVHeTBtWVBVZ0lnIiwic3ViIjoic2JzdGcybmFwcCIsImV4cCI6MTU5NTc3ODc4OSwiYXVkIjoiY3BjLVJPUEMtbW9iaWxlIn0.WtLRXKKst_TrZqX2nen41oGs5vP30H6IdCADd2JL_o3zHM454G6fKkzQedb73vP6wkfuk9EDIPIh1GpkS8BsG9OTSwVlnuptFfGusGjSbQQzD62SlghuEmRwKlmHreHUs_qnzLe7wFMSfrdQoJThgAriY9zo2tMMq-KqlQlS0dU4esMhziR0VmBkqgi48RYAPY83ZwGVFaFF7hrCcuEyeNqIUNwUUrWdot3KIdXkOJ4vwDbBPpr9uknBYEnPO6_I7m8dIByqThXLMeVMIMbDbx7wGlLi_stlQSEwwJuTBeP81j04Kyd_gR08sKWnZzEuWK1Bl5_M6Z1OcTHYwzRCkQ'; // Should be an async function that handles token refresh
+    await 'eyJraWQiOiI4WWhSaWRVRjUtNGpMVkFGVWw1V3ZIZU5ENUhGZXJPWDBxOEltY2hTUmZBIiwiYWxnIjoiUlMyNTYifQ.eyJydF9oYXNoIjoiOVd4N1BrZHFhTXhrc1dXUjV6QTZzZyIsImlhdCI6MTU5NTc5MTEwNCwiaXNzIjoiaHR0cHM6Ly9zdGcwMDItd3d3LmVwb3N0LmNhL21nYS9zcHMvb2F1dGgvb2F1dGgyMC9tZXRhZGF0YS9jcGMtUk9QQyIsImF0X2hhc2giOiJnQzVEUlZDbkpQOTZNb2hxTnFSWi1nIiwic3ViIjoic2JzdGcybmFwcCIsImV4cCI6MTU5NTc5ODMwNCwiYXVkIjoiY3BjLVJPUEMtbW9iaWxlIn0.fnF37_k6QZ62m1BYdH1faIwnkDpc2tiwzAx69tc724LyqWRp7mRgYpxZ4aFyjj0nWinqokThN10f6rXHtS_o6ZKik5Ml-gTaczxkFtmx0WqIGP5QxsYLnKVwYI1FdtRe7qsGMRbo8r5honl00DC9h5OC8m93QGKsmwUhubY_NworgS3RR5JIvqQcch1Tfez_DDUuQhgLxY4XM9JohjIHPRJrBGDHJG2QUIhrjdDer1hdm4mJ2dq3sbAxIBYMtt31TNaWaxb6qiyutfJbr8CPhsM-5pMruwJ3ICrfANkMbKErzxSns-FTBsl8QdbE-fO0Ub8vrNm5h5OZqMQxzYd9vg'; // Should be an async function that handles token refresh
 
   client = new AWSAppSyncClient({
     url: awsconfig.aws_appsync_graphqlEndpoint,
@@ -41,13 +41,26 @@ export class AppComponent implements OnInit {
     // });
 
     const ListTrackItem = gql`
-      query {
-        listTrackItems {
+      query ListTrackItems(
+        $filter: ModelTrackItemFilterInput
+        $limit: Int
+        $nextToken: String
+      ) {
+        listTrackItems(filter: $filter, limit: $limit, nextToken: $nextToken) {
           items {
             id
             pin
             description
+            type
+            owner
+            createdAt
+            _version
+            _deleted
+            _lastChangedAt
+            updatedAt
           }
+          nextToken
+          startedAt
         }
       }
     `;
@@ -60,6 +73,9 @@ export class AppComponent implements OnInit {
         console.log(evt);
         this.todos = evt.data.listTrackItems.items;
       });
+
+    this.subscribeTodo();
+    this.updateTodo();
   }
 
   createTodo() {
@@ -74,6 +90,13 @@ export class AppComponent implements OnInit {
           id
           pin
           description
+          type
+          owner
+          createdAt
+          _version
+          _deleted
+          _lastChangedAt
+          updatedAt
         }
       }
     `;
@@ -86,7 +109,88 @@ export class AppComponent implements OnInit {
           description: 'angular',
         },
       });
-      console.log(result.data.createTodo);
+      console.log('item created ' + JSON.stringify(result.data));
     })();
+  }
+
+  updateTodo() {
+    const updateItemQuery = gql`
+      mutation UpdateTrackItem(
+        $input: UpdateTrackItemInput!
+        $condition: ModelTrackItemConditionInput
+      ) {
+        updateTrackItem(input: $input, condition: $condition) {
+          id
+          pin
+          description
+          type
+          owner
+          createdAt
+          _version
+          _deleted
+          _lastChangedAt
+          updatedAt
+        }
+      }
+    `;
+
+    const updateItem = (async () => {
+      const result = await this.client.mutate({
+        mutation: updateItemQuery,
+        variables: {
+          input: {
+            id: '17602d8d-9b27-47a4-beb7-cb08641f5a39',
+            pin: '1231232',
+            description: 'updated description',
+          },
+        },
+      });
+      console.log('item updated ' + JSON.stringify(result.data));
+    })();
+  }
+
+  subscribeTodo() {
+    const subscribeToDo = gql`
+      subscription OnCreateTrackItem($owner: String!) {
+        onCreateTrackItem(owner: $owner) {
+          id
+          pin
+          description
+          type
+          owner
+          createdAt
+          _version
+          _deleted
+          _lastChangedAt
+          updatedAt
+        }
+      }
+    `;
+
+    let subscription;
+
+    console.log('subscribing');
+    (async () => {
+      subscription = this.client
+        .subscribe({
+          query: subscribeToDo,
+          variables: {
+            owner: 'sbstg2napp',
+          },
+        })
+        .subscribe({
+          next: (data) => {
+            console.log('created listener ' + data);
+          },
+          error: (error) => {
+            console.warn(error);
+          },
+        });
+    })();
+
+    // Unsubscribe after 10 secs
+    setTimeout(() => {
+      subscription.unsubscribe();
+    }, 10000);
   }
 }
